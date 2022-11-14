afbb136307fc262c912d2a1bf8bdc4df30828ae1
veerkaur321 <118209190+veerkaur321@users.noreply.github.com>
2022-11-15 04:19:32 +0530

    Add files via upload

---
+++ gmail_login.py
@@ -0,0 +1,39 @@
+#GMAIL LOGIN CODE:
+
+print("Welcome to gamil login !!!")
+
+# first_name=input("enter your first name: ")
+# if first_name>"a" or  first_name<"z" or first_name>"A" or first_name>"Z":
+#     print("next")
+#     last_name=input("enter your last name: ")
+#     if last_name>"a" or  last_name<"z" or last_name>"A" or last_name>"Z":
+#         print("next")
+#         mob=input("enter your mobile number: ")
+#         if len(mob)==10:
+#             print("next")
+#             age=int(input("enter your age: "))
+#             if age>=18 and age<=100:
+#                 print("ok you can login!!!!")
+#                 print("OTP has been sent to you")
+#                 otp=input("enter your otp: ")
+#                 if len (otp)==6 :
+#                     print("correct!")
+#                     gender=input("enter your gender: ")
+#                     if gender=="female" or gender=="male":
+#                         print("ok thanks")
+#                         print("Login Succesful !!!!!","\n")
+
+
+#                         print("Welcome in gmail login! ")
+#                     else:
+#                         print("choose between male or female")
+#                 else: 
+#                     print("invaild otp!!")
+#             else:
+#                 print("sorry you can't login!")        
+#         else:
+#             print("incorrect no.")
+#     else:
+#         print(" please enter vailed name")
+# else:
+#     print("enter your name")
